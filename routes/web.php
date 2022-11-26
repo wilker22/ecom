f5d1240e0e637cbf88889c36eaf57bed36187ba6
wilker22 <wilker.silva@codevasf.gov.br>
2022-11-26 18:02:33 -0300

    layout backendadmin, admin/login

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,11 @@ require __DIR__ . '/auth.php';
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
 });
 
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
 });
+
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
