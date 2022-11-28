a97e972a3dc3b19b382daee117117bbf7597f7e7
wilker22 <wilker.silva@codevasf.gov.br>
2022-11-27 22:59:56 -0300

    photo upload

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,6 +27,40 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/admin/login');
+    }
+
+    public function AdminProfile()
+    {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        //dd($adminData);
+
+        return view('admin.admin_profile_view', compact('adminData'));
+    }
+
+    public function AdminProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            unlink(public_path('upload/admin_images/'.$data->photo));
+            $filename = date('dmYHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ require __DIR__ . '/auth.php';
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
 });
 
 Route::middleware(['auth', 'role:vendor'])->group(function () {
